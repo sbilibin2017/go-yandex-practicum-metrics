3b6533b44f0ae13c415e96b77eaf3324259f1ada
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-03-24 08:23:55 +0300

    Updated metric errors

--- internal/errors/metric.go
+++ internal/errors/metric.go
@@ -3,6 +3,12 @@ package errors
 import "errors"
 
 var (
-	ErrMetricInternal = errors.New("internal error")
-	ErrMetricNotFound = errors.New("metric not found")
+	ErrMetricInternal      = errors.New("internal error")
+	ErrMetricNotFound      = errors.New("metric not found")
+	ErrMetricTypeRequired  = errors.New("metric type required")
+	ErrInvalidMetricType   = errors.New("metric counter or gauge required")
+	ErrMetricNameRequired  = errors.New("metric name required")
+	ErrMetricValueRequired = errors.New("metric value required")
+	ErrInvalidCounterValue = errors.New("metric invalid counter value")
+	ErrInvalidGaugeValue   = errors.New("metric invalid gauge value")
 )
