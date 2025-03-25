910b97ace38a920395a5d99aa491c4cecfed3ea7
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-03-25 03:57:10 +0300

    Removed getters and setters from configs

--- internal/configs/agent.go
+++ internal/configs/agent.go
@@ -1,52 +1,11 @@
 package configs
 
-import (
-	"strconv"
-)
-
 type AgentConfig struct {
-	address        string
-	reportInterval string
-	pollInterval   string
+	Address        string
+	ReportInterval string
+	PollInterval   string
 }
 
 func NewAgentConfig() *AgentConfig {
 	return &AgentConfig{}
-}
-
-// Getters
-func (a *AgentConfig) GetAddress() string {
-	return a.address
-}
-
-func (a *AgentConfig) GetReportInterval() int {
-	interval, _ := strconv.Atoi(a.reportInterval)
-	return interval
-}
-
-func (a *AgentConfig) GetPollInterval() int {
-	interval, _ := strconv.Atoi(a.pollInterval)
-	return interval
-}
-
-// Setters
-func (a *AgentConfig) SetAddress(address string) bool {
-	a.address = address
-	return true
-}
-
-func (a *AgentConfig) SetReportInterval(reportInterval string) bool {
-	if _, err := strconv.Atoi(reportInterval); err != nil {
-		return false
-	}
-	a.reportInterval = reportInterval
-	return true
-}
-
-func (a *AgentConfig) SetPollInterval(pollInterval string) bool {
-	if _, err := strconv.Atoi(pollInterval); err != nil {
-		return false
-	}
-	a.pollInterval = pollInterval
-	return true
 }
--- internal/configs/server.go
+++ internal/configs/server.go
@@ -1,71 +1,13 @@
 package configs
 
-import (
-	"strconv"
-)
-
 type ServerConfig struct {
-	address         string
-	databaseDSN     string
-	storeInterval   string
-	fileStoragePath string
-	restore         string
+	Address         string
+	DatabaseDSN     string
+	StoreInterval   string
+	FileStoragePath string
+	Restore         string
 }
 
 func NewServerConfig() *ServerConfig {
 	return &ServerConfig{}
-}
-
-// Getters
-func (s *ServerConfig) GetAddress() string {
-	return s.address
-}
-
-func (s *ServerConfig) GetDatabaseDSN() string {
-	return s.databaseDSN
-}
-
-func (s *ServerConfig) GetStoreInterval() int {
-	interval, _ := strconv.Atoi(s.storeInterval)
-	return interval
-}
-
-func (s *ServerConfig) GetFileStoragePath() string {
-	return s.fileStoragePath
-}
-
-func (s *ServerConfig) GetRestore() bool {
-	return s.restore == "true"
-}
-
-// Setters
-func (s *ServerConfig) SetAddress(address string) bool {
-	s.address = address
-	return true
-}
-
-func (s *ServerConfig) SetDatabaseDSN(databaseDSN string) bool {
-	s.databaseDSN = databaseDSN
-	return true
-}
-
-func (s *ServerConfig) SetStoreInterval(storeInterval string) bool {
-	if _, err := strconv.Atoi(storeInterval); err != nil {
-		return false
-	}
-	s.storeInterval = storeInterval
-	return true
-}
-
-func (s *ServerConfig) SetFileStoragePath(fileStoragePath string) bool {
-	s.fileStoragePath = fileStoragePath
-	return true
-}
-
-func (s *ServerConfig) SetRestore(restore string) bool {
-	if restore != "true" && restore != "false" {
-		return false
-	}
-	s.restore = restore
-	return true
 }
